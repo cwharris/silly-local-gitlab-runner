ab4113c13653d0558aa059c03006e5b598d31043
Christopher Harris <xixonia@gmail.com>
2023-08-16 15:44:06 -0500

    use docker-from-docker

--- runner.sh
+++ runner.sh
@@ -2,16 +2,26 @@ set -Eeox pipefail
 
 cd $(dirname "$(realpath "$0")")
 
+# kill and remove the existing container if not already done
 docker stop gitlab-runner || :
 docker container rm gitlab-runner || :
+
+# start the gitlab runner
 docker run -d \
   --name gitlab-runner \
-  -v $PWD/etc/gitlab-runner:/etc/gitlab-runner \
   -v $PWD:$PWD \
   -v /var/run/docker.sock:/var/run/docker.sock \
   gitlab/gitlab-runner:latest
+
+# config gitlab runner to trust this repo
 docker exec -it -w $PWD gitlab-runner git config --global --add safe.directory "*"
-docker exec -it -w $PWD gitlab-runner ls /etc/gitlab-runner
-docker exec -it -w $PWD gitlab-runner gitlab-runner exec docker --docker-privileged build
+
+# run the build step of the .gitlab-ci.yml
+docker exec -it -w $PWD gitlab-runner gitlab-runner exec docker \
+    --docker-privileged \
+    --docker-volumes /var/run/docker.sock:/var/run/docker.sock \
+    build
+
+# kill and remove the existing container
 docker stop gitlab-runner
 docker container rm gitlab-runner
